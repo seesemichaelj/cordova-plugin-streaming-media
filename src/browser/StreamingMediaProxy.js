81b8d8002d1717aabcf073f51fb2ced25f726986
Mike Seese <seesemichaelj@gmail.com>
2017-06-22 14:15:27 -0400

    implement video player

--- src/browser/StreamingMediaProxy.js
+++ src/browser/StreamingMediaProxy.js
@@ -1,16 +1,48 @@
 
 "use strict";
 
-const exec = (success, error, opts) => {
+function StreamingMedia() {
+	jQuery.ajax({
+		"url": "http://vjs.zencdn.net/6.1.0/video.js",
+		"dataType": "script",
+		"async": true,
+		"success": () => {
+		}
+	});
+
+	const videoTag = "<video style=\"display: none;\" id=\"streamingMediaVideo\" data-setup=\"{}\" controls=\"\"></video>";
+	$("body").append(videoTag);
+}
+
+const playAudio = (url, options) => {
 	//
 };
 
-const playAudio = (success, error, opts) => {
+const pauseAudio = (options) => {
 	//
+};
+
+const resumeAudio = (options) => {
+	//
+};
+
+const stopAudio = (options) => {
+	//
+};
+
+const playVideo = (url, options) => {
+	$("#streamingMediaVideo").show();
+	let player = videojs("streamingVideoPlayer");
+	player.src(url);
+	player.play();
 };
 
 module.exports = {
-	"exec": exec
+	"playAudio": playAudio,
+	"pauseAudio": pauseAudio,
+	"resumeAudio": resumeAudio,
+	"stopAudio": stopAudio,
+	"playVideo": playVideo
 };
 
 require("cordova/exec/proxy").add("StreamingMedia", module.exports);
